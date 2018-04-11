5c62c2795e5368e287992945d3e4265e2d7abb22
LiamAumann <ChumCoding@gmail.com>
2018-04-10 19:03:14 -0500

    Updates to app.py
    added answers route

--- activategames/activategames.py
+++ activategames/activategames.py
@@ -7,5 +7,22 @@ app = Flask(__name__)
 def index():
     return render_template('HomePage.html')
 
+
+@app.route('/')
+@app.route('/Jeopardy')
+def jeopardy():
+    return render_template('Jeopardy.html')
+
+
+@app.route('/')
+@app.route('/FF')
+def fam_feud():
+    return render_template('FF.html')
+
+@app.route('/')
+@app.route('/answers')
+def answers():
+    return render_template('answer.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
